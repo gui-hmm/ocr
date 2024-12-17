4a4cf152ced5e0c3c008fc19e94560740a8f2deb
gui-hmm <henriquemelomoura@gmail.com>
2024-12-17 16:10:27 -0300

    fix: ajuste para deploy

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.NEXT_PUBLIC_API_URL,
+    origin: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
